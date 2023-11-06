6a42365a22ac1bc8c1e1da3562e3244253e94a3d
satani99 <sataninikhil@gmail.com>
2023-11-06 20:58:05 +0530

    Updated

--- vector_quantize_tinygrad/vector_quantize_tinygrad.py
+++ vector_quantize_tinygrad/vector_quantize_tinygrad.py
@@ -4,4 +4,20 @@ import tinygrad
 import numpy as np 
 from tinygrad.tensor import Tensor 
 from tinygrad import nn
-import einops 
+from einops import rearrange, repeat, reduce, pack, unpack
+
+from typing import Callable
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d 
+
+def noop(*args, **kwargs):
+    pass 
+
+def identity(t):
+    return t 
+
+
