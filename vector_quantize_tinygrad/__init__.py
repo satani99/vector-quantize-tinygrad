60a97f69732b665057039236af8a86f3a1f6e934
satani99 <sataninikhil@gmail.com>
2023-11-06 19:51:18 +0530

    Changes to be committed: 	new file:   vector_quantize_tinygrad/__init__.py 	new file:   vector_quantize_tinygrad/vector_quantize_tinygrad.py

---
+++ vector_quantize_tinygrad/__init__.py
@@ -0,0 +1,1 @@
+from vector_quantize_tinygrad.vector_quantize_tinygrad import VectorQuantize
---
+++ vector_quantize_tinygrad/vector_quantize_tinygrad.py
@@ -0,0 +1,7 @@
+from functools import partial 
+
+import tinygrad 
+import numpy as np 
+from tinygrad.tensor import Tensor 
+from tinygrad import nn
+import einops 
