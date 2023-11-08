67e42bc588f1fc10e9aecd49cc7c3c7704e1e470
satani99 <sataninikhil@gmail.com>
2023-11-09 02:25:57 +0530

    Upto gumbel sample

--- vector_quantize_tinygrad/vector_quantize_tinygrad.py
+++ vector_quantize_tinygrad/vector_quantize_tinygrad.py
@@ -20,8 +20,8 @@ def noop(*args, **kwargs):
 def identity(t):
     return t 
 
-def normalize(t, p=2, dim=1, eps=1e-12):
-    norm = np.linalg.norm(t.numpy(), ord=p, axis=dim, keepdim=True)
+def normalize(t, p=2, axis=1, eps=1e-12):
+    norm = np.linalg.norm(t.numpy(), ord=p, axis=axis, keepdim=True)
     norm += eps 
     normalized_t = t / norm 
     return Tensor(normalized_t)
@@ -37,6 +37,79 @@ def cdlist(x, y):
 
 def log(t, eps=1e-20):
     return t.clip(min_=eps, max_=float("inf")).log()
+
+def lerp(input, end, weight):
+    out = input + weight * (end - input)
+    return out 
+
+def ema_inplace(old, new, decay):
+    is_mps = str(old.device).startswith('mps:')
+
+    if not is_mps:
+        old = lerp(old, new, 1-decay)
+    else: 
+        old = (old * decay) + (new * (1 - decay))
+
+def pack_one(t, pattern):
+    return Tensor(pack([t.numpy()], pattern))
+
+def unpack_one(t, ps, pattern):
+    return Tensor(unpack(t.numpy(), ps, pattern)[0])
+
+def uniform_init(*shape):
+    t = Tensor.kaiming_uniform(shape)
+    return t 
+
+def gumbel_noise(t):
+    noise = Tensor.uniform(*t.shape)
+    return -log(-log(noise))
+
+def one_hot(arr, num_classes, dtype):
+    one_hot = np.zeros((len(arr), num_classes), dtype=dtype)
+    one_hot[np.arange(len(arr)), arr] = 1
+    return one_hot
+
+def gumbel_sample(
+    logits,
+    temperature = 1.,
+    stochastic = False,
+    straight_through = False,
+    reinmax = False,
+    axis = -1,
+    training = True 
+):
+    dtype, size = logits.dtype, logits.shape[dim]
+
+    if training and stochastic and temperature > 0:
+        sampling_logits = (logits / temperature) + gumbel_noise(logits)
+    else:
+        sampling_logits = logits 
+
+    ind = sampling_logits.argmax(axis=axis)
+    one_hot = Tensor(one_hot(list(ind), size, dtype))
+
+    assert not (reinmax and not straight_through), 'reinmax can only be turned on if using straight through gumbel softmax'
+
+    if not straight_through or temperature <= 0. or not training:
+        return ind, one_hot 
+
+    # use reinmax for better second-order accuracy - https://arxiv.org/abs/2304.08612
+    # algorithm 2
+
+    if reinmax:
+        pi0 = logits.softmax(axis=axis)
+        pi1 = (one_hot + (logits / temperature).softamx(axis=axis)) / 2
+        pi1 = ((log(pi1) - logits).detach() + logits).softmax(axis=axis)
+        pi2 = 2 * pi1 - 0.5 * pi0 
+        one_hot = pi2 - pi2.detach() + one_hot 
+    else:
+        pi1 = (logits / temperature).softmax(axis=axis)
+        one_hot = one_hot + pi1 - pi1.detach()
+
+    return ind, one_hot
+
+
+
 
 
 
