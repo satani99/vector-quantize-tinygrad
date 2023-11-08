b9ecce956c3e4fa85d8e4ede8a8a1895fcc7b848
satani99 <sataninikhil@gmail.com>
2023-11-08 23:51:49 +0530

    modified:   vector_quantize_tinygrad/vector_quantize_tinygrad.py

--- vector_quantize_tinygrad/vector_quantize_tinygrad.py
+++ vector_quantize_tinygrad/vector_quantize_tinygrad.py
@@ -20,4 +20,24 @@ def noop(*args, **kwargs):
 def identity(t):
     return t 
 
+def normalize(t, p=2, dim=1, eps=1e-12):
+    norm = np.linalg.norm(t.numpy(), ord=p, axis=dim, keepdim=True)
+    norm += eps 
+    normalized_t = t / norm 
+    return Tensor(normalized_t)
+
+def l2norm(t):
+    return normalize(t, p=2, dim=-1)
+
+def cdlist(x, y):
+    x2 = reduce(x.numpy() ** 2, 'b n d -> b n', 'sum')
+    y2 = reduce(y.numpy() ** 2, 'b n d -> b n', 'sum')
+    xy = np.einsum('b i d, b j d -> b i j', x.numpy(), y.numpy()) * -2 
+    return (Tensor(rearrange(x2, 'b i -> b i 1') + rearrange(y2, 'b j -> b 1 j') + xy)).sqrt()
+
+def log(t, eps=1e-20):
+    return t.clip(min_=eps, max_=float("inf")).log()
+
+
+
 
